bec0038aee97f629fc3f2746a19bea206fc507a6
DylanKersting <dylankersting@gmail.com>
2021-09-22 16:32:56 -0500

    add temlate nav bar

--- src/client/templates/Recipelist/index.js
+++ src/client/templates/Recipelist/index.js
@@ -11,23 +11,36 @@ export const Main = () => {
 
         <header className="header">
             <div className="container">
-                <nav className="navbar navbar-inverse navbar-toggleable-md">
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#RecipeListmenu" aria-controls="RecipeListmenu" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse justify-content-md-center" id="RecipeListmenu">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a className="nav-link" href="food-index.html">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="food-category.html">Recipes</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="food-contact.html">Contact Me</a>
-                            </li>  
-                        </ul>
-                    </div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a className="dropdown-item" href="#">Action</a>
+                        <a className="dropdown-item" href="#">Another action</a>
+                        <div className="dropdown-divider"></div>
+                        <a className="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                    </ul>
+                </div>
                 </nav>
             </div>
         </header>
